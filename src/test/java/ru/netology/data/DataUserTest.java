91189198f3f0256111ff9576b6bd4eb7c73f55f1
Tatyana Hripkova <tkhripkova@bk.ru>
2021-02-15 23:54:10 +0200

    Задание первое

---
+++ src/test/java/ru/netology/data/DataUserTest.java
@@ -0,0 +1,57 @@
+package ru.netology.data;
+
+import com.codeborne.selenide.SelenideElement;
+import com.github.javafaker.Faker;
+import lombok.Value;
+import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Random;
+
+
+class DataUserTest {
+    static Faker faker = new Faker(new Locale("ru"));
+
+    private DataUserTest() {
+    }
+
+    static String cityForInput() {
+        Random random = new Random();
+        int rand = random.nextInt(5);
+        String city[] = { "Калининград", "Санкт-Петербург", "Москва", "Сеул"};
+        return city[rand];
+    }
+
+    static String cityNoVal() {
+        Random random = new Random();
+        int rand = random.nextInt(3);
+        String cityNoVal[] = {"Ставрополь", "Ростов-на-Дону"};
+        return cityNoVal[rand];
+    }
+
+    static String dataPhone() {
+        String phone = faker.phoneNumber().phoneNumber();
+        return phone;
+    }
+
+    static String dataName() {
+        String name = faker.name().lastName();
+        name = name + " " + faker.name().firstName();
+        return name;
+    }
+
+    static String dataNameWishLetterEBrief() {
+        Random random = new Random();
+        int rand = random.nextInt(4);
+        String nameWishEBrief[] = {"Александра", "Анастасия", "Татьяна" };
+        return nameWishEBrief[rand];
+    }
+
+    static String dateMeeting(int days) {
+        String dateMeeting = LocalDate.now().plusDays(17).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+        return dateMeeting;
+    }
+
+}
---
+++ src/test/java/ru/netology/data/OrderDataUserTest.java
@@ -0,0 +1,89 @@
+package ru.netology.data;
+
+import com.codeborne.selenide.SelenideElement;
+import lombok.val;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selenide.*;
+import static com.codeborne.selenide.Selenide.$;
+
+class OrderDataUserTest {
+
+    void dataInput(int days) {
+        SelenideElement data = $("[data-test-id=date]");
+        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        data.$("[placeholder]").setValue(DataUserTest.dateMeeting(days));
+    }
+
+    @BeforeEach
+    void before() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void getTrueInputValidForm() {
+        $("[placeholder=Город]").setValue(DataUserTest.cityForInput());
+        int inDays = 5;
+        dataInput(inDays);
+        $("[data-test-id=phone]").$("[name=phone]").setValue(DataUserTest.dataPhone());
+        $("[data-test-id=name].input_type_text .input__control").setValue(DataUserTest.dataName());
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Запланировать")).click();
+        $("[data-test-id=success-notification]").$("[class=notification__content]")
+                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + DataUserTest.dateMeeting(inDays)));
+        $$("[class=button__text]").find(exactText("Запланировать")).click();
+        $$("[class=button__text]").find(exactText("Перепланировать")).click();
+        $("[data-test-id=success-notification]").$("[class=notification__content]")
+                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + DataUserTest.dateMeeting(inDays)));
+    }
+
+    @Test
+    void errorExpectedWhenInputIncorrectCity() {
+        $("[placeholder=Город]").setValue(DataUserTest.cityNoVal());
+        int inDays = 5;
+        dataInput(inDays);
+        $("[data-test-id=phone]").$("[name=phone]").setValue(DataUserTest.dataPhone());
+        $("[data-test-id=name].input_type_text .input__control").setValue(DataUserTest.dataName());
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Запланировать")).click();
+        $("[data-test-id=city] .input__sub").shouldHave
+                (exactTextCaseSensitive("Доставка в выбранный город недоступна"));
+    }
+
+    @Test
+    void errorExpectedWhenEmptyFieldDate() {
+        $("[placeholder=Город]").setValue(DataUserTest.cityForInput());
+        SelenideElement data = $("[data-test-id=date]");
+        data.$("[value]").doubleClick().sendKeys(Keys.BACK_SPACE);
+        $("[data-test-id=phone]").$("[name=phone]").setValue(DataUserTest.dataPhone());
+        $("[data-test-id=name].input_type_text .input__control").setValue(DataUserTest.dataName());
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Запланировать")).click();
+        $("[data-test-id=date] .input__sub").shouldHave
+                (exactTextCaseSensitive("Неверно введена дата"));
+    }
+
+    @Test
+    void getTrueWishLetterEBriefInName() {
+        $("[placeholder=Город]").setValue(DataUserTest.cityForInput());
+        int inDays = 5;
+        dataInput(inDays);
+        $("[data-test-id=phone]").$("[name=phone]").setValue(DataUserTest.dataPhone());
+        $("[data-test-id=name].input_type_text .input__control").setValue(DataUserTest.dataNameWishLetterEBrief());
+        $("[class=checkbox__box]").click();
+        $$("[class=button__text]").find(exactText("Запланировать")).click();
+        $("[data-test-id=success-notification]").$("[class=notification__content]")
+                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + DataUserTest.dateMeeting(inDays)));
+        $$("[class=button__text]").find(exactText("Запланировать")).click();
+        $$("[class=button__text]").find(exactText("Перепланировать")).click();
+        $("[data-test-id=success-notification]").$("[class=notification__content]")
+                .shouldHave(textCaseSensitive("Встреча успешно запланирована на " + DataUserTest.dateMeeting(inDays)));
+    }
+}
